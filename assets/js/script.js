49518a8c408a8bd760519417f3cb7db33d006c93
RenataLimaTED <relferreira@senacrs.com.br>
2023-10-24 12:09:39 -0300

    dev tela introducao em andamento

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,50 @@
+function escalaProporcao(largura, altura) {
+
+    var larguraScreen = $(window).width();
+    var alturaScreen = $(window).height();
+    var proporcaoAltura = (alturaScreen * 100) / altura;
+    var proporcaoLargura = (larguraScreen * 100) / largura;
+    var proporcao, larguraAltura, larguraAlturaAuto;
+
+    if (proporcaoAltura < proporcaoLargura) {
+        larguraAltura = "height";
+        larguraAlturaAuto = "width";
+        proporcao = proporcaoAltura / 100;
+    } else {
+        larguraAltura = "width";
+        larguraAlturaAuto = "height";
+        proporcao = proporcaoLargura / 100;
+    }
+
+    return [proporcao, larguraAltura, larguraAlturaAuto];
+}
+
+function resizeBodyConteudo() {
+
+    var proporcao1920 = escalaProporcao(1920, 1080)[0];
+
+    $(".conteudo").css({
+        "transform": "scale(" + proporcao1920 + ")",
+        "transform-origin": "center center"
+    });
+
+    var proporcao900;
+
+    if ($(window).width() < 992) {
+        proporcao900 = escalaProporcao(900, 576)[0];
+    } else {
+        proporcao900 = 1;
+    }
+}
+
 $(document).ready(function () {
+
+    resizeBodyConteudo()
+    $(window).resize(function () {
+        resizeBodyConteudo()
+    })
+
+
     function verificarValor(id, valorCorreto) {
         $(id).on('blur', function () {
             var valorDigitado = parseFloat($(this).val().replace(',', '.'));
@@ -23,3 +69,4 @@ $(document).ready(function () {
     verificarValor('#acougue-sabado', 20.00);
     verificarValor('#acougue-domingo', 20.00);
 });
+
